b87073e1c25ff7812500516c59fa1463f7b346cb
Matthew Banse-Fay <matthew.bansefay@gmail.com>
2021-11-07 12:35:16 -0800

    Update form behavior to use custom data attributes to calculate score.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
   $("#survey-form").submit(function(event) {
     event.preventDefault();
-    const favAnimal = $(":radio[name=fav-animal]:checked").val();
-    const favColor = $(":radio[name=fav-color]:checked").val();
-    const favBeachWalkLength = $(":radio[name=fav-beach-walk-length]:checked").val();
-    const favLocation = $(":radio[name=fav-location]:checked").val();
-    const favBeverage = $(":radio[name=fav-beverage]:checked").val();
+    const favAnimal = $(":radio[name=fav-animal]:checked").data("language");
+    const favColor = $(":radio[name=fav-color]:checked").data("language");
+    const favBeachWalkLength = $(":radio[name=fav-beach-walk-length]:checked").data("language");
+    const favLocation = $(":radio[name=fav-location]:checked").data("language");
+    const favBeverage = $(":radio[name=fav-beverage]:checked").data("language");
+
+    const favArray = [favAnimal, favColor, favBeachWalkLength, favLocation, favBeverage];
 
     let rubyScore = 0;
     let javascriptScore = 0;
@@ -16,45 +18,15 @@ $(document).ready(function() {
     if (favAnimal === undefined || favColor === undefined || favBeachWalkLength === undefined || favLocation === undefined || favBeverage === undefined) {
       alert("Please fill out the full form!");
     } else {
-      if (favAnimal === "rat") {
-        javascriptScore += 1;
-      } else if (favAnimal === "cat") {
-        rubyScore += 1;
-      } else if (favAnimal === "snake") {
-        pythonScore += 1;
-      }
-  
-      if (favColor === "blue") {
-        javascriptScore += 1;
-      } else if (favColor === "red") {
-        rubyScore += 1;
-      } else if (favColor === "green") {
-        pythonScore += 1;
-      }
-  
-      if (favBeachWalkLength === "medium") {
-        javascriptScore += 1;
-      } else if (favBeachWalkLength === "short") {
-        rubyScore += 1;
-      } else if (favBeachWalkLength === "long") {
-        pythonScore += 1;
-      }
-  
-      if (favLocation === "city") {
-        javascriptScore += 1;
-      } else if (favLocation === "beach") {
-        rubyScore += 1;
-      } else if (favLocation === "mountain") {
-        pythonScore += 1;
-      }
-  
-      if (favBeverage === "tea") {
-        javascriptScore += 1;
-      } else if (favBeverage === "coffee") {
-        rubyScore += 1;
-      } else if (favBeverage === "soda") {
-        pythonScore += 1;
-      }
+      favArray.forEach(function(choice) {
+        if (choice === "javascript") {
+          javascriptScore += 1;
+        } else if (choice === "ruby") {
+          rubyScore += 1;
+        } else if (choice === "python") {
+          pythonScore += 1;
+        }
+      });
   
       if (javascriptScore >= rubyScore && javascriptScore >= pythonScore) {
         $("#javascript").fadeToggle();
