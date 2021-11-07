c24f405a7308d4ae6bd88af0adbbda200160cbd5
Matthew Banse-Fay <matthew.bansefay@gmail.com>
2021-11-06 20:09:54 -0700

    Add behavior to show a language based on form input, add behavior to alert about empty entries, add further styling to header and languages.

--- js/scripts.js
+++ js/scripts.js
@@ -11,12 +11,60 @@ $(document).ready(function() {
     let javascriptScore = 0;
     let pythonScore = 0;
 
-    if (favAnimal === "rat") {
-      javascriptScore += 1;
-    } else if (favAnimal === "cat") {
-      rubyScore += 1;
-    } else if (favAnimal === "snake") {
-      pythonScore += 1;
+    $(".programming-language").hide();
+
+    console.log(favAnimal);
+
+    if (favAnimal === undefined || favColor === undefined || favBeachWalkLength === undefined || favLocation === undefined || favBeverage === undefined) {
+      alert("Please fill out the full form!");
+    } else {
+      if (favAnimal === "rat") {
+        javascriptScore += 1;
+      } else if (favAnimal === "cat") {
+        rubyScore += 1;
+      } else if (favAnimal === "snake") {
+        pythonScore += 1;
+      }
+  
+      if (favColor === "blue") {
+        javascriptScore += 1;
+      } else if (favColor === "red") {
+        rubyScore += 1;
+      } else if (favColor === "green") {
+        pythonScore += 1;
+      }
+  
+      if (favBeachWalkLength === "medium") {
+        javascriptScore += 1;
+      } else if (favBeachWalkLength === "short") {
+        rubyScore += 1;
+      } else if (favBeachWalkLength === "long") {
+        pythonScore += 1;
+      }
+  
+      if (favLocation === "city") {
+        javascriptScore += 1;
+      } else if (favLocation === "beach") {
+        rubyScore += 1;
+      } else if (favLocation === "mountain") {
+        pythonScore += 1;
+      }
+  
+      if (favBeverage === "tea") {
+        javascriptScore += 1;
+      } else if (favBeverage === "coffee") {
+        rubyScore += 1;
+      } else if (favBeverage === "soda") {
+        pythonScore += 1;
+      }
+  
+      if (javascriptScore >= rubyScore && javascriptScore >= pythonScore) {
+        $("#javascript").slideToggle();
+      } else if (rubyScore >= javascriptScore && rubyScore >= pythonScore) {
+        $("#ruby").slideToggle();
+      } else if (pythonScore >= javascriptScore && pythonScore >= rubyScore) {
+        $("#python").slideToggle();
+      }
     }
   });
 });
